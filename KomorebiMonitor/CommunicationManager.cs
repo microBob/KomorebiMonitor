b20c9c8ce22d0e7f25c22f1a9b2388286154b131
Kenneth Yang <microbob@is88.com>
2025-07-21 22:37:10 -0700

    1 react to focus change events (#2)
    * WIP remove some duplicates
    
    * Converted schemas
    
    * Namespace separation of models
    
    * Add README, begin startup, not connecting to named pipe
    
    * Komorebi not reacting to socket messages
    
    * Moved named pipe creation to constructor, add socket print
    
    * Used namespace, there's a race condition with attaching
    
    * Parse and trigger logic, inconsistent subscribe request
    
    ---------
    
    Co-authored-by: Kenneth Yang <kjy@is88.com>

---
+++ KomorebiMonitor/CommunicationManager.cs
@@ -0,0 +1,120 @@
+﻿using System.IO.Pipes;
+using System.Net.Sockets;
+using System.Text;
+using KomorebiMonitor.NotificationModel;
+using KomorebiMonitor.SocketModel;
+using NotificationTypeEnum = KomorebiMonitor.NotificationModel.TypeEnum;
+using SocketContent = KomorebiMonitor.SocketModel.Content;
+using SocketTypeEnum = KomorebiMonitor.SocketModel.TypeEnum;
+
+namespace KomorebiMonitor;
+
+public class CommunicationManager
+{
+    #region Properties
+
+    private readonly string _komorebiNamedPipeName;
+
+    private readonly NamedPipeServerStream _namedPipeServer;
+    private readonly NetworkStream _networkStream;
+
+    #endregion
+
+    public CommunicationManager(
+        string komorebiSocketAddress,
+        int komorebiSocketPort,
+        string komorebiNamedPipeName
+    )
+    {
+        _komorebiNamedPipeName = komorebiNamedPipeName;
+
+        // Start a background thread to listen for Komorebi events on the named pipe.
+        _namedPipeServer = new NamedPipeServerStream(_komorebiNamedPipeName, PipeDirection.In);
+        var pipeThread = new Thread(ListenForKomorebiEvents) { IsBackground = true };
+        pipeThread.Start();
+
+        // Start a socket connection to the Komorebi server to send commands.
+        using var client = new TcpClient();
+        client.Connect(komorebiSocketAddress, komorebiSocketPort);
+        Console.WriteLine("Connected to Komorebi socket server.");
+        _networkStream = client.GetStream();
+        RequestKomorebiSubscribeToNamedPipe();
+
+        while (true)
+        {
+            try
+            {
+                if (_networkStream.DataAvailable)
+                {
+                    var buffer = new byte[4096];
+                    var bytesRead = _networkStream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine(response);
+                    }
+                }
+                Thread.Sleep(100); // avoid busy loop
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading from socket: {ex.Message}");
+            }
+        }
+    }
+
+    private void ListenForKomorebiEvents()
+    {
+        _namedPipeServer.WaitForConnection();
+        Console.WriteLine("Komorebi connected to named pipe.");
+
+        using var streamReader = new StreamReader(_namedPipeServer, Encoding.UTF8);
+        while (true)
+        {
+            try
+            {
+                while (streamReader.ReadLine() is { } line)
+                {
+                    var asMessage = NotificationMessage.FromJson(line);
+                    if (asMessage.Event.WindowManagerEvent.Type
+                        is not (NotificationTypeEnum.FocusChange
+                        or NotificationTypeEnum.FocusWorkspaceNumber)) continue;
+                    var monitor = asMessage.State.Monitors.Focused;
+                    var workspace = asMessage
+                        .State
+                        .Monitors
+                        .Elements[monitor]
+                        .Workspaces
+                        .Focused;
+
+                    Console.WriteLine($"{monitor}: {workspace}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    private void RequestKomorebiSubscribeToNamedPipe()
+    {
+        // Create message to subscribe to named pipe.
+        var message = new SocketMessage
+        {
+            Content = new SocketContent { String = _komorebiNamedPipeName },
+            Type = SocketTypeEnum.AddSubscriberPipe,
+        };
+        var data = Encoding.UTF8.GetBytes(message.ToJson());
+
+        _networkStream.Write(data, 0, data.Length);
+        Console.WriteLine("Sent named pipe message");
+    }
+
+    public void OnApplicationExit()
+    {
+        _namedPipeServer.Dispose();
+        _networkStream.Dispose();
+    }
+}
